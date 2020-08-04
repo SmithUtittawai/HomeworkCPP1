23eec1e9913499b2424ce3f94384d4777ccb2fce
SmithUtittawai <69200537+SmithUtittawai@users.noreply.github.com>
2020-08-04 20:39:45 +0700

    Add files via upload

---
+++ Homework1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>;
+
+using namespace std;
+
+void main() {
+	cout<<"     #    "<<endl;
+	cout<<"   #   #  "<<endl;
+	cout<<"  #     # "<<endl;
+	cout<<" #       #"<<endl;
+	cout<<" #########"<<endl;
+	cout<<" #       #"<<endl;
+	cout<<"Smith Utittawai\n";
+	system("pause");
+}
